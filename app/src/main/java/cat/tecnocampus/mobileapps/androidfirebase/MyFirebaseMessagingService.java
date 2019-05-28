63a64d3aef9a44247be4987c71c382d66c7ef7bc
lmartinezn <lmartinezn@edu.tecnocampus.cat>
2019-05-28 09:43:06 +0200

    Added sendNotification method

--- app/src/main/java/cat/tecnocampus/mobileapps/androidfirebase/MyFirebaseMessagingService.java
+++ app/src/main/java/cat/tecnocampus/mobileapps/androidfirebase/MyFirebaseMessagingService.java
@@ -1,6 +1,13 @@
 package cat.tecnocampus.mobileapps.androidfirebase;
 
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -31,11 +38,47 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
-            Log.d("SwA", "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            String body = remoteMessage.getNotification().getBody();
+
+            Log.d("SwA", "Message Notification Body: " + body);
+
+            sendNotification(body);
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+    private void sendNotification(String body) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_ONE_SHOT
+        );
+
+        Uri defaultUriSong = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(
+                this,
+                MainActivity.CHANNEL_ID
+        );
+
+        notificationBuilder
+                .setSmallIcon(android.R.drawable.stat_notify_chat)
+                .setContentTitle("Test Noti")
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(MainActivity.NOTIFICATION_ID, notificationBuilder.build());
     }
 
     /**
